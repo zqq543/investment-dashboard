12fc5f5995e734820b61acc8ae31ffb4becdfa91
Dashboard Deploy <deploy@dashboard.local>
2026-05-03 20:47:48 +0800

    update: dashboard 2026-05-03 20:47

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -16,7 +16,7 @@ export default function manifest(): MetadataRoute.Manifest {
         src: '/app-icon.svg',
         sizes: 'any',
         type: 'image/svg+xml',
-        purpose: 'any maskable',
+        purpose: 'maskable',
       },
     ],
   }
